7aaa513141f3626027ab1a3c4e94734386dcffe0
Florian Drews <info@florian-drews.de>
2014-01-24 15:39:21 +0100

    WWB-34 Daten über API in Postgresql DB speichern
    * Read Out Weatherdata

--- api/resources.py
+++ api/resources.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from tastypie.authorization import Authorization
 from tastypie import fields
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-from .models import Weatherstation
+from .models import Weatherstation, Weatherdata
 
 
 class UserResource(ModelResource):
@@ -10,20 +10,32 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         resource_name = 'user'
         excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
-        """authentication = BasicAuthentication"""
         filtering = {
             'username': ALL,
         }
 
 
-class EntryResource(ModelResource):
-    user = fields.ForeignKey(UserResource, 'user')
-
+class WsResource(ModelResource):
     class Meta:
         queryset = Weatherstation.objects.all()
+        resource_name = 'weatherstation'
+        excludes = ['user']
+        filtering = {
+            'name': ALL,
+        }
+        object_class = 'name'
+
+
+class EntryResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+    weatherstation = fields.ForeignKey(WsResource, 'weatherstation')
+
+    class Meta:
+        queryset = Weatherdata.objects.all()
+        """queryset = User.objects.all()"""
         resource_name = 'weatherdata'
         authorization = Authorization()
         filtering = {
             'user': ALL_WITH_RELATIONS,
-            'Weatherstation': ALL,
+            'weatherstation': ALL_WITH_RELATIONS,
         }
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import *
 from tastypie.api import Api
-from .resources import EntryResource, UserResource
+from .resources import UserResource,EntryResource, WsResource
 
 v1_api = Api(api_name='v1')
+v1_api.register(WsResource())
 v1_api.register(UserResource())
 v1_api.register(EntryResource())
 
