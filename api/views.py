237d16700219263dcca7793224aba08fea1290cd
Florian Drews <info@florian-drews.de>
2014-02-26 14:17:47 +0100

    WWB-34 Daten über API in Postgresql DB speichern
    * Code cleaner

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse, Http404
-from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 
@@ -17,9 +16,7 @@ def detail(request, ws_id):
         wd = Weatherdata.objects.filter(weatherstation_id=ws.id).all().order_by('-timestamp')
     except Weatherstation.DoesNotExist:
         raise Http404
-    t = loader.get_template('api/detail.html')
-    c = Context({'weatherdata': wd})
-    return HttpResponse(t.render(c))
+    return render_to_response('api/detail.html', {'weatherdata': wd})
 
 
 def user_ws(request, user_id):
@@ -27,10 +24,12 @@ def user_ws(request, user_id):
         user = User.objects.get(id=user_id)
         weatherstation = Weatherstation.objects.select_related('user__weatherdata').filter(user_id=user_id).all()
         weather_dict = {}
-        for ws in weatherstation:
-            wd = Weatherdata.objects.select_related('weatherstation__weatherdata').filter(weatherstation_id=ws.id).all()
-            weather_dict[ws] = wd
-
+        if Weatherstation.objects.filter(user_id=user_id):
+            for ws in weatherstation:
+                wd = Weatherdata.objects.select_related('weatherstation__weatherdata').filter(weatherstation_id=ws.id).all()
+                weather_dict[ws] = wd
+        else:
+            return HttpResponse('Keine Wetterstationen verfügbar')
     except User.DoesNotExist:
         raise Http404
     context = {'weather_dict': weather_dict, 'user': user}
